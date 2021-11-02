607fc4c9d09f72ec8cacf08a166f44ab950037e2
jingtaozhan <jingtaozhan@qq.com>
2021-11-02 15:28:08 +0800

    fix links

--- cmds/download_query_encoder.sh
+++ cmds/download_query_encoder.sh
@@ -35,26 +35,26 @@ query_encoder_doc_dir="doc/official_query_encoders "
 mkdir -p $query_encoder_doc_dir
 cd $query_encoder_doc_dir
 
-wget https://www.dropbox.com/s/jym4juvd0j98w5x/m48.marcopass.query.encoder.tar.gz?dl=0 -O m4.marcopass.query.encoder.tar.gz
-tar -xzvf m4.marcopass.query.encoder.tar.gz
+wget https://www.dropbox.com/s/1atamawqr3f8wx6/m4.marcodoc.query.encoder.tar.gz?dl=0 -O m4.marcodoc.query.encoder.tar.gz
+tar -xzvf m4.marcodoc.query.encoder.tar.gz
 
-wget https://www.dropbox.com/s/uisl6el92o8y9g5/m8.marcodoc.query.encoder.tar.gz?dl=0 -O m8.marcopass.query.encoder.tar.gz
-tar -xzvf m8.marcopass.query.encoder.tar.gz
+wget https://www.dropbox.com/s/uisl6el92o8y9g5/m8.marcodoc.query.encoder.tar.gz?dl=0 -O m8.marcodoc.query.encoder.tar.gz
+tar -xzvf m8.marcodoc.query.encoder.tar.gz
 
-wget https://www.dropbox.com/s/nzmrzbp5r4ghhct/m12.marcodoc.query.encoder.tar.gz?dl=0 -O m12.marcopass.query.encoder.tar.gz
-tar -xzvf m12.marcopass.query.encoder.tar.gz
+wget https://www.dropbox.com/s/nzmrzbp5r4ghhct/m12.marcodoc.query.encoder.tar.gz?dl=0 -O m12.marcodoc.query.encoder.tar.gz
+tar -xzvf m12.marcodoc.query.encoder.tar.gz
 
-wget https://www.dropbox.com/s/vwk0r2rfhht7ntd/m16.marcodoc.query.encoder.tar.gz?dl=0 -O m16.marcopass.query.encoder.tar.gz
-tar -xzvf m16.marcopass.query.encoder.tar.gz
+wget https://www.dropbox.com/s/vwk0r2rfhht7ntd/m16.marcodoc.query.encoder.tar.gz?dl=0 -O m16.marcodoc.query.encoder.tar.gz
+tar -xzvf m16.marcodoc.query.encoder.tar.gz
 
-wget https://www.dropbox.com/s/rb2cc6bkj226n0z/m24.marcodoc.query.encoder.tar.gz?dl=0 -O m24.marcopass.query.encoder.tar.gz
-tar -xzvf m24.marcopass.query.encoder.tar.gz
+wget https://www.dropbox.com/s/rb2cc6bkj226n0z/m24.marcodoc.query.encoder.tar.gz?dl=0 -O m24.marcodoc.query.encoder.tar.gz
+tar -xzvf m24.marcodoc.query.encoder.tar.gz
 
-wget https://www.dropbox.com/s/l5ww8hy44kyk7bz/m32.marcodoc.query.encoder.tar.gz?dl=0 -O m32.marcopass.query.encoder.tar.gz
-tar -xzvf m32.marcopass.query.encoder.tar.gz
+wget https://www.dropbox.com/s/l5ww8hy44kyk7bz/m32.marcodoc.query.encoder.tar.gz?dl=0 -O m32.marcodoc.query.encoder.tar.gz
+tar -xzvf m32.marcodoc.query.encoder.tar.gz
 
-wget https://www.dropbox.com/s/bsz4v3qu9w3pyuq/m48.marcodoc.query.encoder.tar.gz?dl=0 -O m48.marcopass.query.encoder.tar.gz
-tar -xzvf m48.marcopass.query.encoder.tar.gz
+wget https://www.dropbox.com/s/bsz4v3qu9w3pyuq/m48.marcodoc.query.encoder.tar.gz?dl=0 -O m48.marcodoc.query.encoder.tar.gz
+tar -xzvf m48.marcodoc.query.encoder.tar.gz
 
 
 
